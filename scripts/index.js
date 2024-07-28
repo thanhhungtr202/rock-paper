b0ade0e971d874a6e4a6c4c074fcd3a7d9ac82d1
Thanh Hung Tran <thanhhungtr202@gmail.com>
2024-07-28 07:29:30 -0600

    Finished adding logics for the game

--- scripts/index.js
+++ scripts/index.js
@@ -5,28 +5,45 @@ let computerChoice = () => {
 
 let playerScore = 0;
 let computerScore = 0;
+let result = document.createElement("p");
+result.setAttribute("id", "result");
+let containerQuery = document.querySelector(".container");
+containerQuery.appendChild(result);
+let resultOutput = document.querySelector("#result");
 
 //lmao these are just the para men, still have to actually pass the function when calling play(para1, para2) lol
-let play = (computerC, humanC) => {
-    for(let i = 0; i < 5; i++){
-        let computerMove = computerC();
-        let humanMove = humanC();
+let play = (e, computerC) => {
+    let computerMove = computerC();
+    let humanMove = e.target.id;
 
-        if((computerMove == "rock" && humanMove == "scissors") || (computerMove == "scissors" && humanMove == "paper") ||
-        (computerMove == "paper" && humanMove == "rock")){
-            computerScore++;
-            console.log("Computer Score: " + computerScore);
-        }
-        else if((humanMove == "rock" && computerMove == "scissors") || (humanMove == "scissors" && computerMove == "paper") ||
-        (humanMove == "paper" && computerMove == "rock")){
-            playerScore++;
-            console.log("Player Score: " + playerScore);
+    if((computerMove == "rock" && humanMove == "scissors") || (computerMove == "scissors" && humanMove == "paper") ||
+    (computerMove == "paper" && humanMove == "rock")){
+        computerScore++;
+        resultOutput.textContent = "Computer Won - Score: " + computerScore;
+    }
+    else if((humanMove == "rock" && computerMove == "scissors") || (humanMove == "scissors" && computerMove == "paper") ||
+    (humanMove == "paper" && computerMove == "rock")){
+        playerScore++;
+        resultOutput.textContent = "Player Won - Score: " + playerScore;
+    }
+    else{
+        resultOutput.textContent = "It's a draw!";
+    }
+    if(playerScore == 5 || computerScore == 5){
+        if(playerScore > computerScore){
+            resultOutput.textContent = "Player Won"
         }
         else{
-            console.log("It's a draw!");
+            resultOutput.textContent = "Computer Won"
         }
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
-//pass in the function lmao, how else is it gonna access it if the para is undefined
-play(computerChoice, humanChoice);
+const selections = document.querySelectorAll(".choice");
+selections.forEach((selection) => {
+    selection.addEventListener("click", function(e) {
+        play(e, computerChoice);
+    });
+});
